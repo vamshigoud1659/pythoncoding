20fe7bee209c90533a9ce48364eff9c07f8fb1df
vamshigoud1659 <vamshigoud1659@gmail.com>
2026-09-07 21:52:08 +0530

    update

--- day2.py
+++ day2.py
@@ -26,40 +26,40 @@
 # b = int(input("enter b="))
 # c = int(input("enter c="))
 
-# if a==b==c:
-#   print(0)
-# elif a==b:
-#   print(c)
-# elif b==c:
-#   print(a)
-# elif c==a:
-#   print(b)
+# # if a==b==c:
+# #   print(0)
+# # elif a==b:
+# #   print(c)
+# # elif b==c:
+# #   print(a)
+# # elif c==a:
+# #   print(b)
+# # else:
+# #   print(a+b+c)
+
+
+# speed = int(input("Enter speed= "))
+# birthday = int(input("enter 1 or 0: "))
+
+# if birthday:
+#     if speed<=65:
+#         print("no ticket")
+
+#     elif 65<=speed<=85:
+#         print("small ticket")
+
+#     else:
+#         print("big ticket")
+
 # else:
-#   print(a+b+c)
-
-
-speed = int(input("Enter speed= "))
-birthday = int(input("enter 1 or 0: "))
-
-if birthday:
-    if speed<=65:
-        print("no ticket")
-
-    elif 65<=speed<=85:
-        print("small ticket")
-
-    else:
-        print("big ticket")
-
-else:
-    if speed<=60:
-        print("no ticket")
+#     if speed<=60:
+#         print("no ticket")
     
-    elif 60<=speed<=80:
-        print("small ticket")
+#     elif 60<=speed<=80:
+#         print("small ticket")
    
-    else:
-        print("big ticket")
+#     else:
+#         print("big ticket")
    
    
 
---
+++ day4.py
@@ -0,0 +1,54 @@
+# num = int(input("enter a num:"))
+# if num>0:
+#     print("positive")
+# else:
+#     print("negative")
+
+# num = int(input("enter number:"))
+
+# if num%2==0:
+#     print(num,"is even")
+# else:
+#     print(num,"is odd")
+
+# num = int(input("enter a num:"))
+
+# if num>0:
+#     print("positive")
+# elif num<0:
+#     print("negative")
+# else:
+#     print("zero")
+
+# marks= int(input("enter marks="))
+# if marks>90:
+#     print("A Grade")
+# elif 75<=marks<=89:
+#     print("B Grade")
+# elif 60<=marks<=74:
+#     print("C Grade")
+# elif 40<=marks<=59:
+#     print("D Grade")
+# else:
+#     print("fail")
+
+# num = int(input("Enter a Number:"))
+# if num==1:
+#     print("Monday")
+# elif num==2:
+#     print("Tuesday")
+# elif num==3:
+#     print("Wednesday")
+# elif num==4:
+#     print("Thursday")
+# elif num==5:
+#     print("Friday")
+# elif num==6:
+#     print("Saturday")
+# elif num==7:
+#     print("Sunday")
+# else:
+#     print("Invalid")
+
+
+
