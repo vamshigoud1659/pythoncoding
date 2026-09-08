3e04f32867bef56ae019f9e05aab744555451601
vamshigoud1659 <vamshigoud1659@gmail.com>
2026-09-08 14:57:01 +0530

    update

--- day5.py
+++ day5.py
@@ -1,11 +1,10 @@
-age=int(input("enter num="))
+# age=int(input("enter num="))
 
-if age>=18:
-    license=input("Do you have license:")
-    if license=='yes':
-         print("Eligible for driving")
-    else:
-        print("GET YOUR LICENSE")
-else:
-    print("NOT eligible for driving")
-    
+# if age>=18:
+#     license=input("Do you have license:")
+#     if license=='yes':
+#          print("Eligible for driving")
+#     else:
+#         print("GET YOUR LICENSE")
+# else:
+#     print("NOT eligible for driving")
