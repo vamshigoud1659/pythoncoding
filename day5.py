4e303f8c59cfc0abff11e08cef44b95cfaac47ff
vamshigoud1659 <vamshigoud1659@gmail.com>
2026-09-09 11:15:00 +0530

    update

--- day5.py
+++ day5.py
@@ -7,4 +7,37 @@
 #     else:
 #         print("GET YOUR LICENSE")
 # else:
-#     print("NOT eligible for driving")cle
+#     print("NOT eligible for driving")
+
+# attendence=int(input("enter your attendence="))
+# if attendence>=75:
+#     marks=int(input("enter marks="))
+#     if marks>=40:
+#         print("Eligible for scholarship")
+#     else:
+#         print("Not Eligible")
+# else:
+    #  print("attendence shortage")
+
+# for i in range(5):
+#     print("python")
+
+# for i in range(5):
+#     print(i)
+
+# for i in range(101):
+#        print(i)
+
+# for i in range(1,6):
+#          print(i)
+
+# for i in range(1,10,3):
+#          print(i)
+
+# n=int(input("enter n="))
+# for i in range(2,n,2):
+#     print(i)
+
+# n=int(input("enter num="))
+# print("even numbers are:")
+# for i in range(2,n+1,2)
