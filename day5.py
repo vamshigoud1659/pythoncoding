eb20c9a67ea97edee472fb1722a7482ef6cecc51
vamshigoud1659 <vamshigoud1659@gmail.com>
2026-09-10 11:27:17 +0530

    update

--- day5.py
+++ day5.py
@@ -95,18 +95,3 @@
 # for i in range(1,n+1):
 #     factorial=factorial*i
 # print(factorial)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ day6.py
@@ -0,0 +1,41 @@
+# n=int(input("Enter a num="))
+# for i in range(1,n+1):
+#    if i%5==0:
+#     print(i)
+
+# n=int(input("enter num="))
+# count=0
+# for i in range(1,n+1):
+#     if i%5==0:
+#      count=count+1
+# print(count)
+
+# name="python"
+# for i in name:
+#     print(i)
+
+# n=input("Enter Num=")
+# sum=0
+# for i in n:
+#     sum=sum+int(i)
+# print(sum)
+
+# n=int(input("How Many Numbers you want to enter="))
+# for i in range(1,n+1):
+#     b=int(input("enter num="))
+#     print(b)
+
+# n=int(input("Enter a Num="))
+# biggest_number=0
+# for i in range(1,n+1):
+#     b=int(input("enter numbers:"))
+#     if b>biggest_number:
+#         biggest_number=b
+# print(biggest_number)
+
+
+
+     
+
+
+    
