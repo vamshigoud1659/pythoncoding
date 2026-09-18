a5b5358dc0ae4fa800cd13b8666c9aec52265a03
vamshigoud1659 <vamshigoud1659@gmail.com>
2026-09-18 19:09:11 +0530

    update

--- day7.py
+++ day7.py
@@ -26,3 +26,44 @@
 #     if i%5==0:
 #         continue
 #     print(i)
+
+# for i in range(1,11):
+#     if i==3:
+#         continue
+#     if i==7:
+#         break
+#     print(i)
+
+# i=1
+# while i<=10:
+#      print(i)
+#      i += 1
+
+# i=2
+# while i<=10:
+#      print(i)
+#      i += 2
+
+# i=1
+# while i<=10:
+#     if i%2==0: 
+#         print(i)
+#     i+=1
+
+# i=10
+# while i>=1:
+#     print(i)
+#     i -= 1
+
+# n=int(input("Enter a Number="))
+# i=0
+# sum=0
+# while i<=n:
+#     if i%2==0:
+#         sum=sum+i
+#     i+=1
+# print(sum)
+
+
+
+
---
+++ day8.py
@@ -0,0 +1,20 @@
+# name="python"
+# print(name[5])
+
+# name="python"
+# print(name[2:])
+
+# name="***PYTHON***"
+# print(name.strip("*"))
+
+# name="$$$PYTHON$$$"
+# print(name.lstrip("$"))
+
+# name="$$$PYTHON$$$"
+# print(name.rstrip("$"))
+
+# name="C language"
+# print(name.replace("C language","Python"))
+
+# name="C language"
+# print(name.replace("C language","Python"))
